fc7a353a37fb701fe02cb7d905ed1526ddfd163c
dev-sincerity <kllliv50@gmail.com>
2023-03-06 10:12:20 +0900

    bitbucket 테스트1

---
+++ jenkins-test2/src/test/java/com/example/jenkinstest2/JenkinsTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
